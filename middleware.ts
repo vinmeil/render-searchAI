27f0c57dbf4101b38c8638d255e35b338861569b
Vincent <vincent.wesley.liem@gmail.com>
2024-12-23 18:01:52 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware({
+export default authMiddleware({
   publicRoutes: [
     "/",
     // "/events/:id",
