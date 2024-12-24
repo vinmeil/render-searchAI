8c57a6edf4e099df451042307220a96852cc18c0
Vincent <vincent.wesley.liem@gmail.com>
2024-12-24 11:08:31 +0800

    add chromium path

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,5 +4,6 @@ const { join } = require("path");
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
+  executablePath: "/opt/bin/chromium",
   cacheDirectory: join(__dirname, "node_modules", ".puppeteer_cache"),
 };
