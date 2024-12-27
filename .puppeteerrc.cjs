485aa777d418ae6f7770bc192c0db8fdbf8ebc12
Vincent <vincent.wesley.liem@gmail.com>
2024-12-27 10:05:04 +0800

    pls

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,5 @@ const { join } = require("path");
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-  executablePath: "/opt/bin/chromium",
   cacheDirectory: join(__dirname, "node_modules", ".puppeteer_cache"),
 };
