b45ab501fa5fb606b9817d319ba943e8626eb6f6
Vincent <vincent.wesley.liem@gmail.com>
2024-12-29 16:58:45 +0800

    call BE api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getProducts } from "./backend/app";
 
 export async function POST(req: NextRequest) {
   const { query } = await req.json();
   console.log("Query inside POST", query);
-  const products = await getProducts(query as string);
-  return NextResponse.json(products);
+
+  try {
+    const res = await fetch(
+      "https://searchai-backend-docker.onrender.com/search",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Error fetching products: ${res.statusText}`);
+    }
+
+    const products = await res.json();
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
--- next.config.ts
+++ next.config.ts
@@ -3,42 +3,6 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.resolve(__dirname, "app/api/products/backend"),
-              to: path.resolve(
-                __dirname,
-                ".next/server/app/api/products/backend"
-              ),
-            },
-            {
-              from: path.resolve(
-                __dirname,
-                "node_modules/puppeteer-extra-plugin-stealth/evasions"
-              ),
-              to: path.resolve(
-                __dirname,
-                ".next/server/node_modules/puppeteer-extra-plugin-stealth/evasions"
-              ),
-            },
-            {
-              from: path.resolve(__dirname, "node_modules/puppeteer"),
-              to: path.resolve(
-                __dirname,
-                ".next/server/node_modules/puppeteer"
-              ),
-            },
-          ],
-        })
-      );
-    }
-    return config;
-  },
   images: {
     remotePatterns: [
       {
